d450887115578ec9d2e811fc246deac882df738e
chaijiang <chaijiangchaisan@gmail.com>
2025-04-26 20:11:58 +0700

    fix:ex1-6

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,13 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for(let i=companyName.length-1;i>=0;i--){
+    reversedCompanyName+=(companyName[i]);
+}
+let reversedCompanyName2 = [];
+for (let c=0;c<=companyName.length-1;c++){
+    reversedCompanyName2.unshift(companyName[c]);
+}
 console.log(reversedCompanyName);
+console.log(reversedCompanyName2);
+
